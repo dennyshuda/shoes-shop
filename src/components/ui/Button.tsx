b731857bda5757423a90f62ca05cc2039e554b78
Denny Setyawan Huda <denycam9@gmail.com>
2024-10-06 00:09:46 +0700

    fix: add variant

--- src/components/ui/Button.tsx
+++ src/components/ui/Button.tsx
@@ -1,10 +1,19 @@
 import { cn } from "@/lib/utils";
 
-type ButtonProps = React.ComponentProps<"button">;
+type ButtonProps = React.ComponentProps<"button"> & {
+	variant?: "secondary";
+};
 
-const Button: React.FC<ButtonProps> = ({ className, ...props }) => {
+const Button: React.FC<ButtonProps> = ({ className, variant, ...props }) => {
 	return (
-		<button className={cn("bg-black text-white w-full rounded-md py-2.5", className)} {...props} />
+		<button
+			className={cn(
+				"bg-black text-white w-full rounded-md py-2.5",
+				{ "bg-white text-black": variant === "secondary" },
+				className
+			)}
+			{...props}
+		/>
 	);
 };
 
